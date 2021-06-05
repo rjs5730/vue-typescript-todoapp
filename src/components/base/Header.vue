c11b5e5f1adaee3c7dd65f42792d27e2a1fbae9e
seunghyun <rjs5730@naver.com>
2021-06-05 17:25:23 +0900

    [#1] feat: localStroage todoItem 조회 기능 추가

---
+++ src/components/base/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>{{ headerText }}</h1>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      headerText: "Vue-Typescript-TodoApp",
+    };
+  },
+});
+</script>
+
+<style scoped></style>
